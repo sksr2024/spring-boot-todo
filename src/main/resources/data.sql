5405d4aca9327db324e0b651544bd0d07cbb0167
sksr2024 <guancaili3@gmail.com>
2025-07-17 08:22:58 +0900

    変更

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO tasks (summary, description, status) VALUES ('Spring Boot を学ぶ', 'TODO アプリを作る', 'DONE');
+INSERT INTO tasks (summary, description, status) VALUES ('Spring Security を学ぶ', 'ログイン機能をを作る', 'TODO');
