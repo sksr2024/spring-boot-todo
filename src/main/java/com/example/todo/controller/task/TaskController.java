ee184473fbfb46ff987bac8a3e2f11f0326d496e
sksr2024 <guancaili3@gmail.com>
2025-07-20 18:45:06 +0900

    feat: エラーページの実装・新規作成フォームページを途中まで作成

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -29,5 +30,16 @@ public class TaskController {
                 .orElseThrow(() -> new IllegalArgumentException("Task not found: id = " + taskId));
         model.addAttribute("task", TaskDTO.toDTO(taskEntity));
         return "tasks/detail";
+    }
+
+    // GET /tasks/creationForm
+    @GetMapping("/tasks/creationForm")
+    public String showCreationForm(){
+        return "tasks/form";
+    }
+
+    @PostMapping("/tasks")
+    public String create(Model model){
+        return list(model);
     }
 }
